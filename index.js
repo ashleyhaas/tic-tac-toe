d8fa1bf2fdf5eff415fab334ddae2a6f22f5188e
Peter Mayor <peter@avironsoftware.com>
2022-03-30 16:47:09 -0400

    refactor to be more dynamic

--- index.js
+++ index.js
@@ -1,68 +1,128 @@
-let currentPlayer = "X"
-let clickCounter = 0
-let isGameOver = false
-let resetDiv = document.querySelector("#reset")
-let resetButtonElement = document.createElement("button")
-resetButtonElement.innerHTML = "Reset"
-resetButtonElement.addEventListener("click", function () {
-  window.location.reload()
-})
-function checkForWin() {
-  let box1 = document.querySelector("#box-1").innerHTML
-  let box2 = document.querySelector("#box-2").innerHTML
-  let box3 = document.querySelector("#box-3").innerHTML
-  let box4 = document.querySelector("#box-4").innerHTML
-  let box5 = document.querySelector("#box-5").innerHTML
-  let box6 = document.querySelector("#box-6").innerHTML
-  let box7 = document.querySelector("#box-7").innerHTML
-  let box8 = document.querySelector("#box-8").innerHTML
-  let box9 = document.querySelector("#box-9").innerHTML
+let currentPlayer = "X";
+let clickCounter = 0;
+let isGameOver = false;
+let resetDiv = document.querySelector("#reset");
+let resetButtonElement = document.createElement("button");
 
-  if ((box1 === "X" && box4 === "X" && box7 === "X") || 
-  (box2 === "X" && box5 === "X" && box8 === "X") || 
-  (box3 === "X" && box6 === "X" && box9 === "X") || 
-  (box1 === "X" && box5 === "X" && box9 === "X") || 
-  (box3 === "X" && box5 === "X" && box7 === "X") ||
-  (box1 === "X" && box2 === "X" && box3 === "X") ||
-  (box4 === "X" && box5 === "X" && box6 === "X") ||
-  (box7 === "X" && box8 === "X" && box9 === "X")) {
-    setTimeout(function () { alert("X Wins!")}, 200);
-    isGameOver = true
-    resetDiv.appendChild(resetButtonElement)
-  } if ((box1 === "O" && box4 === "O" && box7 === "O") || 
-  (box2 === "O" && box5 === "O" && box8 === "O") || 
-  (box3 === "O" && box6 === "O" && box9 === "O") || 
-  (box1 === "O" && box5 === "O" && box9 === "O") || 
-  (box3 === "O" && box5 === "O" && box7 === "O") ||
-  (box1 === "O" && box2 === "O" && box3 === "O") ||
-  (box4 === "O" && box5 === "O" && box6 === "O") ||
-  (box7 === "O" && box8 === "O" && box9 === "O")) {
-    setTimeout(function () { alert("O Wins!")}, 200);
-    isGameOver = true
-    resetDiv.appendChild(resetButtonElement)
-  } else if (clickCounter > 8) {
-    setTimeout(function () { alert("It was a draw! Try again.")}, 200);
-    isGameOver = true
-    resetDiv.appendChild(resetButtonElement)
+resetButtonElement.innerHTML = "Reset";
+resetButtonElement.addEventListener("click", function () {
+  window.location.reload();
+});
+
+function checkForWin() {
+  //   let box1 = document.querySelector("#box-1").innerHTML;
+  //   let box2 = document.querySelector("#box-2").innerHTML;
+  //   let box3 = document.querySelector("#box-3").innerHTML;
+  //   let box4 = document.querySelector("#box-4").innerHTML;
+  //   let box5 = document.querySelector("#box-5").innerHTML;
+  //   let box6 = document.querySelector("#box-6").innerHTML;
+  //   let box7 = document.querySelector("#box-7").innerHTML;
+  //   let box8 = document.querySelector("#box-8").innerHTML;
+  //   let box9 = document.querySelector("#box-9").innerHTML;
+
+  const boardArray = [null];
+
+  for (let i = 1; i <= 9; i++) {
+    const currentBoxHTML = document.querySelector("#box-" + i).innerHTML;
+    boardArray.push(currentBoxHTML);
   }
+
+  console.log(boardArray);
+
+  function updateDOMAfterWin(winner) {
+    setTimeout(function () {
+      alert(winner + " Wins!");
+    }, 200);
+    isGameOver = true;
+    resetDiv.appendChild(resetButtonElement);
+  }
+
+  function checkColumn(colNum) {
+    const currentColumn = [];
+    for (let i = colNum; i <= 9; i += 3) {
+      const currentBoxHTML = boardArray[i];
+      currentColumn.push(currentBoxHTML);
+    }
+    const firstItemInColumn = currentColumn[0];
+    const hasWon = currentColumn.every(function (currentVal) {
+      if (currentVal === "") {
+        return false;
+      } else {
+        return currentVal === firstItemInColumn;
+      }
+    });
+    return [hasWon, firstItemInColumn];
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    const currentColumnValue = checkColumn(i);
+    const hasWon = currentColumnValue[0];
+    const winner = currentColumnValue[1];
+    if (hasWon) {
+      updateDOMAfterWin(winner);
+    }
+  }
+
+  //   (box1 === "X" && box4 === "X" && box7 === "X") ||
+  //     (box2 === "X" && box5 === "X" && box8 === "X") ||
+  //     (box3 === "X" && box6 === "X" && box9 === "X") ||
+
+  //   if (
+  //     (box1 === "X" && box4 === "X" && box7 === "X") ||
+  //     (box2 === "X" && box5 === "X" && box8 === "X") ||
+  //     (box3 === "X" && box6 === "X" && box9 === "X") ||
+  //     (box1 === "X" && box5 === "X" && box9 === "X") ||
+  //     (box3 === "X" && box5 === "X" && box7 === "X") ||
+  //     (box1 === "X" && box2 === "X" && box3 === "X") ||
+  //     (box4 === "X" && box5 === "X" && box6 === "X") ||
+  //     (box7 === "X" && box8 === "X" && box9 === "X")
+  //   ) {
+  //   setTimeout(function () {
+  //     alert("X Wins!");
+  //   }, 200);
+  //   isGameOver = true;
+  //   resetDiv.appendChild(resetButtonElement);
+  //   }
+  //   if (
+  //     (box1 === "O" && box4 === "O" && box7 === "O") ||
+  //     (box2 === "O" && box5 === "O" && box8 === "O") ||
+  //     (box3 === "O" && box6 === "O" && box9 === "O") ||
+  //     (box1 === "O" && box5 === "O" && box9 === "O") ||
+  //     (box3 === "O" && box5 === "O" && box7 === "O") ||
+  //     (box1 === "O" && box2 === "O" && box3 === "O") ||
+  //     (box4 === "O" && box5 === "O" && box6 === "O") ||
+  //     (box7 === "O" && box8 === "O" && box9 === "O")
+  //   ) {
+  //     setTimeout(function () {
+  //       alert("O Wins!");
+  //     }, 200);
+  //     isGameOver = true;
+  //     resetDiv.appendChild(resetButtonElement);
+  //   } else if (clickCounter > 8) {
+  //     setTimeout(function () {
+  //       alert("It was a draw! Try again.");
+  //     }, 200);
+  //     isGameOver = true;
+  //     resetDiv.appendChild(resetButtonElement);
+  //   }
   //console.log(box1.innerHTML)
 }
 
 function boxClickHandler(event) {
   if (isGameOver === true) {
-    return
+    return;
   }
   if (event.target.innerHTML !== "") {
-    return
+    return;
   }
-  event.target.innerHTML = currentPlayer
-  clickCounter++
+  event.target.innerHTML = currentPlayer;
+  clickCounter++;
   if (currentPlayer === "X") {
-    currentPlayer = "O"
+    currentPlayer = "O";
   } else {
-    currentPlayer = "X"
+    currentPlayer = "X";
   }
-  checkForWin()
+  checkForWin();
   //console.log(event.target);
 }
 
